a9e6006eb33c54db627a196627b4a2c8d52aedd2
SilentPowers <SilentPowers@users.noreply.github.com>
2021-02-23 14:55:03 -0600

    Q1

---
+++ w1d2dataTypes/question_1.js
@@ -0,0 +1,10 @@
+"use-strict"
+//L = US qt x 0.946352946
+//conversion factor
+const num = 0.946352946;
+const prompt = require('prompt-sync')();
+const quarts = prompt("Enter quarts to be converted into liters: ")
+
+const liters = quarts * num;
+
+console.log(liters);

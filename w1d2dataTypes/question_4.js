3766c11acec53e61cca14933e42f153fee5516e8
SilentPowers <SilentPowers@users.noreply.github.com>
2021-02-24 08:11:27 -0600

    Update question_4.js

--- w1d2dataTypes/question_4.js
+++ w1d2dataTypes/question_4.js
@@ -1,7 +1,21 @@
+"use-strict";
+
 // Write JavaScript program to compute the mileage of a vehicle. Your program should allow the
 // user to enter the beginning and ending odometer readings and the number of gallons of gas
 // used and should output the mileage in miles per gallon.
 
 //User enters  begOdometer reading
-//User enters endOdmeter reading
+//User enters endOdometer reading
 //User enters numOfGallons used
+//subtract the enOdometer from the begOdometer
+//divide miles by the gallons
+
+const prompt = require('prompt-sync')();
+const begOdometer = prompt("Enter beginning odometer: ")
+const endOdometer = prompt("Enter ending odometer: ")
+const numOfGallons = prompt("Enter number of gallons used: ")
+
+const totalMiles = Number(endOdometer - begOdometer);
+const mpg = (totalMiles / numOfGallons);
+
+console.log(`${mpg} mpg`);

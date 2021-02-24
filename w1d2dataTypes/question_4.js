89b5097d576bce8b2772cc3d2d51da13148a7244
SilentPowers <SilentPowers@users.noreply.github.com>
2021-02-23 20:51:29 -0600

    Q4 not done yet

---
+++ w1d2dataTypes/question_4.js
@@ -0,0 +1,7 @@
+// Write JavaScript program to compute the mileage of a vehicle. Your program should allow the
+// user to enter the beginning and ending odometer readings and the number of gallons of gas
+// used and should output the mileage in miles per gallon.
+
+//User enters  begOdometer reading
+//User enters endOdmeter reading
+//User enters numOfGallons used

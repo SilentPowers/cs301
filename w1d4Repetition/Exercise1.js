da6bbbd0c51b98fad50a0a427bc554ffb4b772e6
SilentPowers <SilentPowers@users.noreply.github.com>
2021-02-25 19:10:37 -0600

    In class exercie

---
+++ w1d4Repetition/Exercise1.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const prompt = require('prompt-sync')();
+//let age;
+
+
+// Write a loop that keeps on prompting for age until you enter age
+// older than 18
+// • Write both while and do while versions.
+
+// while(age <= 18){
+//   age = +prompt("What is your age? ");
+// }
+// console.log(age);
+
+
+// do{
+//   age = +prompt("what is your age? ")
+// }
+// while(age <= 18);
+// console.log(age);
+
+
+// Make a defining table and program to print out the balance of a
+// savings account that compounds interest monthly. Prompt the user
+// for the
+// • initial amount
+// • annual interest rate
+// • number of years to compound
+/*
+input                         process                                  output
+initial amount           ((mulitply amount * interest) + amount)
+annual interst reate        
+num of years to compound
+*/
+
+// const initialAmount = prompt("Enter current balance: ")
+// const annualInterestRate = prompt("Enter interst rate: ")
+// const numOfYears = prompt("Enter number of months: ")
+
+ 
+
+
+
+
+
+for(let i = 1; i<=5; i++){
+  let row="";
+  for(let j= 1; j<=5; j++){
+  row += j +"\t";
+  }
+  console.log(row);
+  }
+
+
+
+
+
